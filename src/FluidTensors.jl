2fffbcd2ac0319003cf1c8d0a41cf61825be3503
Felipe Alves <favbalves@gmail.com>
2018-09-22 21:30:21 -0400

    Not a dense array

--- src/FluidTensors.jl
+++ src/FluidTensors.jl
@@ -4,7 +4,7 @@ import LinearAlgebra
 
 export Vec, SymTen, symouter, Lie
 
-abstract type AbstractVec{T} <: DenseArray{T,1} end
+abstract type AbstractVec{T} <: AbstractArray{T,1} end
 abstract type AbstractTen{T} <: AbstractArray{T,2} end
 
 include("vectors.jl")
