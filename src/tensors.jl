bc4d1d179b5771a578e33d314db9da1831da559f
Felipe Alves <favbalves@gmail.com>
2018-09-22 21:30:49 -0400

    Uniform scaling sum returning SymTen

--- src/tensors.jl
+++ src/tensors.jl
@@ -37,6 +37,8 @@ SymTen(q,w,e,r,t,y) =
 @inline Base.:+(a::SymTen{T},b::SymTen{T2}) where {T,T2} = 
     @fastmath SymTen{promote_type(T,T2)}(a.xx+b.xx, a.xy+b.xy, a.xz+b.xz, a.yy+b.yy, a.yz + b.yz, a.zz+b.zz)
 
+@inline Base.:-(A::SymTen{T}) where T = SymTen{T}(-A.xx,-A.xy,-A.xz,-A.yy,-A.yz,-A.zz)
+
 @inline Base.:-(a::SymTen{T},b::SymTen{T2}) where {T,T2} = 
     @fastmath SymTen{promote_type(T,T2)}(a.xx-b.xx, a.xy-b.xy, a.xz-b.xz, a.yy-b.yy, a.yz - b.yz, a.zz - b.zz)
 
@@ -52,13 +54,24 @@ SymTen(q,w,e,r,t,y) =
 @inline Base.:(:)(a::SymTen,b::SymTen) = 
     @fastmath muladd(a.xx, b.xx, muladd(a.yy, b.yy, a.zz*b.zz)) + 2muladd(a.xy, b.xy, muladd(a.xz, b.xz, a.yz*b.yz))
 
+@inline Base.:(+)(L::LinearAlgebra.UniformScaling,A::SymTen) =
+    SymTen(A.xx + L.λ, A.xy,A.xz,A.yy + L.λ, A.yz, A.zz + L.λ)
+
+@inline Base.:(+)(A::SymTen, L::LinearAlgebra.UniformScaling) = L+A
+
+@inline Base.:(-)(A::SymTen, L::LinearAlgebra.UniformScaling) =
+    SymTen(A.xx - L.λ, A.xy,A.xz,A.yy - L.λ, A.yz, A.zz - L.λ)
+
+@inline Base.:(-)(L::LinearAlgebra.UniformScaling,A::SymTen) = -(A-L)
+
+
 @inline LinearAlgebra.tr(a::SymTen) =
     @fastmath a.xx + a.yy + a.zz
 
 @inline LinearAlgebra.norm(a::SymTen) = 
     @fastmath sqrt(2(a:a))
 
-@inline LinearAlgebra.dot(a::SymTen{T},b::Vec{T2}) where {T,T2} = 
+@inline LinearAlgebra.dot(a::SymTen{T},b::Vec{T2}) where {T<:Number,T2<:Number} = 
     @fastmath Vec{promote_type(T,T2)}(muladd(a.xx, b.x, muladd(a.xy, b.y, a.xz*b.z)), 
         muladd(a.xy, b.x, muladd(a.yy, b.y, a.yz*b.z)),
         muladd(a.xz, b.x, muladd(a.yz, b.y, a.zz*b.z)))
@@ -66,7 +79,9 @@ SymTen(q,w,e,r,t,y) =
 @inline LinearAlgebra.dot(b::Vec,a::SymTen) = 
     LinearAlgebra.dot(a,b)
 
-@inline symouter(a::Vec,b::Vec) =
+@inline LinearAlgebra.dot(A::AbstractMatrix,v::Vec) = A*v
+
+@inline symouter(a::Vec,b::Vec) = #symmetric part of the outer product of two vectors
     @fastmath SymTen(a.x*b.x,
                      0.5*muladd(a.x,b.y,a.y*b.x),
                      0.5*muladd(a.x,b.z,a.z*b.x),
@@ -74,7 +89,7 @@ SymTen(q,w,e,r,t,y) =
                      0.5*muladd(a.y,b.z,a.z*b.y),
                      a.z*b.z)
 
-@inline Lie(S::SymTen,w::Vec) = 
+@inline Lie(S::SymTen,w::Vec) = #Lie product of Symmetric Tensor and Anti-symmetric tensor(passed as vector)
     @fastmath SymTen(2*(S.xz*w.y - S.xy*w.z),
                      muladd(w.y,S.yz, muladd(w.z,S.xx, -muladd(w.x,S.xz, w.z*S.yy))),
                      muladd(w.x,S.xy, muladd(w.y,S.zz, -muladd(w.y, S.xx, w.z*S.yz))),
