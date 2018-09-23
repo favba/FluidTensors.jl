9a24d2392b4329ba550dc9778b7e974724d2d110
Felipe Alves <favbalves@gmail.com>
2018-09-23 11:48:43 -0400

    Test if correct type is being returned.

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,15 +7,15 @@ using LinearAlgebra
     b = Vec(4.,5.,6.)
 
     @test a == [1.,2.,3.]
-    @test a+b == Vec(5.,7.,9.)
-    @test b-a == Vec(3.,3.,3.)
+    @test a+b === Vec(5.,7.,9.)
+    @test b-a === Vec(3.,3.,3.)
     @test a⋅b ≈ 32.
     @test norm(a) ≈ 3.7416573867739413
     @test norm(im*a) ≈ 3.7416573867739413
-    @test 2*a == Vec(2.,4.,6.)
-    @test a/2 == Vec(0.5,1.,1.5)
-    @test cross(a,b) == Vec(-3.,6.,-3.)
-    @test cross(b,a) == Vec(3.,-6.,3.)
+    @test 2*a === Vec(2.,4.,6.)
+    @test a/2 === Vec(0.5,1.,1.5)
+    @test cross(a,b) === Vec(-3.,6.,-3.)
+    @test cross(b,a) === Vec(3.,-6.,3.)
 end
 
 @testset "Tensor Operations" begin
@@ -24,10 +24,10 @@ end
                 2. 4. 5.;
                 3. 5. 6.]
     B = SymTen(2.,3.,4.,5.,6.,7.)
-    @test A+B == SymTen(3.,5.,7.,9.,11.,13.)
-    @test B-A == SymTen(1.,1.,1.,1.,1.,1.)
-    @test 2*A == SymTen(2.,4.,6.,8.,10.,12.)
-    @test A/2 == SymTen(0.5,1.,1.5,2.,2.5,3.)
+    @test A+B === SymTen(3.,5.,7.,9.,11.,13.)
+    @test B-A === SymTen(1.,1.,1.,1.,1.,1.)
+    @test 2*A === SymTen(2.,4.,6.,8.,10.,12.)
+    @test A/2 === SymTen(0.5,1.,1.5,2.,2.5,3.)
     @test tr(A) == 11.
     @test A:B == tr(A*B)
     @test norm(A) == sqrt(2*tr(A*A))
@@ -37,12 +37,12 @@ end
     a = Vec(1.,2.,3.)
     b = Vec(4.,5.,6.)
     A = SymTen(1.,2.,3.,4.,5.,6.)
-    @test A⋅a == Vec(14.,25.,31.)
+    @test A⋅a === Vec(14.,25.,31.)
     W = [0. a.z -a.y;
          -a.z 0. a.x;
          a.y -a.x 0.]
     @test Lie(A,a) ==  A*W - W*A
-    @test symouter(a,b) == SymTen(4.,6.5,9.,10.,13.5,18.)
+    @test symouter(a,b) === SymTen(4.,6.5,9.,10.,13.5,18.)
 end
 
 @testset "UniformScaling" begin
